1ec00d2ac368acf3eec46b31ef1a20624622cb84
imbekrishna <imbekrishna@gmail.com>
2026-08-09 14:57:42 +0530

    complete exercise 1.5

--- 1-2-the-project/src/index.ts
+++ 1-2-the-project/src/index.ts
@@ -1,11 +1,27 @@
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/serve-static'
 
 const app = new Hono()
 
 const PORT = process.env.PORT ?? 8000
 
 app.get('/', (c) => {
-  return c.text('Hello world!')
+  return c.html(
+    `<!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>The project</title>
+    </head>
+
+    <body>
+        This is the project homepage.
+    </body>
+
+    </html>`
+)
 })
 
 const server = Bun.serve({
