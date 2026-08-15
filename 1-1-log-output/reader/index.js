a3d627e94739aefad910f75ac44c10095a8fa349
imbekrishna <imbekrishna@gmail.com>
2026-08-16 02:41:25 +0530

    completed exercise 1.11

--- 1-1-log-output/reader/index.js
+++ 1-1-log-output/reader/index.js
@@ -1,23 +1,33 @@
 const fs = require('node:fs');
+const { randomUUID } = require('node:crypto');
 const path = require('node:path');
 const http = require('node:http')
 
 // const directory = path.join('/', 'usr', 'src', 'app', 'logs')
 const directory = path.join('/', 'app', 'logs')
-const filePath = path.join(directory, 'log.txt')
+// const filePath = path.join(directory, 'log.txt')
+const counterPath = path.join(directory, 'counter.txt')
 
-function readLog() {
-    if (!fs.existsSync(filePath)) {
-        fs.mkdirSync(path.dirname(filePath), { recursive: true });
-        fs.writeFileSync(filePath, '', 'utf-8');
+
+function createOutput() {
+    const randomString = randomUUID();
+    const date = new Date();
+    const response = `${date.toISOString()}: ${randomString}`;
+    return response
+}
+
+function readLog(readFilePath) {
+    try {
+        return fs.readFileSync(readFilePath, 'utf-8');
+    } catch (error) {
+        return '0'
     }
-
-    return fs.readFileSync(filePath, 'utf-8');
 }
 
 function handleRequest(req, res) {
     res.writeHead(200);
-    res.end(readLog());
+    // res.end(readlog(filePath));
+    res.end(`${createOutput()}\nPing / Pongs: ${readLog(counterPath)}`);
 }
 
 const server = http.createServer(handleRequest);
--- 1-3-ping-pong/index.js
+++ 1-3-ping-pong/index.js
@@ -1,10 +1,27 @@
 const http = require('node:http');
+const fs = require('node:fs');
+const path = require('node:path');
+const os = require('node:os');
+
+const directory = path.join('/', 'app', 'logs')
+const filePath = path.join(directory, 'counter.txt')
+
+function writeLog(output) {
+	if (!fs.existsSync(filePath)) {
+		fs.mkdirSync(path.dirname(filePath), { recursive: true });
+		fs.writeFileSync(filePath, '', 'utf-8');
+	}
+	fs.writeFileSync(filePath, output + os.EOL, 'utf-8');
+}
 
 let count = 0;
 
 function handleRequest(req, res) {
+	count += 1;
+	console.log(count);
 	res.writeHead(200);
-	res.end(`pong ${count++}`);
+	res.end(`pong ${count}`);
+	writeLog(count);
 }
 
 const server = http.createServer(handleRequest);
